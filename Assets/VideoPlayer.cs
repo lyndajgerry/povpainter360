6609250ffddc7f285bd64f955be23a2367110d26
TomasMoesgaard <bsureb@gmail.com>
2016-07-19 14:35:38 +0200

    Initial push containing main scene and leap support minus the video files

---
+++ Assets/VideoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour
+{
+
+		public GameObject leftSphere, rightSphere;
+
+		void Start ()
+		{
+			((MovieTexture)leftSphere.GetComponent<Renderer>().material.mainTexture).Play ();
+			((MovieTexture)rightSphere.GetComponent<Renderer>().material.mainTexture).Play ();
+		}
+
+	}
