9833cc0041029cc54fd36236ad87e552bcc64614
TomasMoesgaard <bsureb@gmail.com>
2016-07-19 17:12:49 +0200

    Added text and changed hand size

--- Assets/VideoPlayer.cs
+++ Assets/VideoPlayer.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VideoPlayer : MonoBehaviour
 {
 
-		public GameObject leftSphere, rightSphere;
+	private bool IsPlaying = false;
 
-		void Start ()
-		{
-			((MovieTexture)leftSphere.GetComponent<Renderer>().material.mainTexture).Play ();
-			((MovieTexture)rightSphere.GetComponent<Renderer>().material.mainTexture).Play ();
+	public enum Handedness { Right, Left};
+
+	public Handedness PaintingHand = Handedness.Right;
+
+	public GameObject leftSphere, rightSphere;
+
+	public GameObject StartText;
+
+	public void StartVideo(int hand){
+
+
+		StartText.SetActive (false);
+
+		if (!IsPlaying) {
+			
+		if (hand == 0) {
+			PaintingHand = Handedness.Left;
+		} else {
+			PaintingHand = Handedness.Right;
 		}
 
+
+
+			((MovieTexture)leftSphere.GetComponent<Renderer> ().material.mainTexture).Play ();
+			((MovieTexture)rightSphere.GetComponent<Renderer> ().material.mainTexture).Play ();
+
+		}
+
+
+		IsPlaying = true;
+
+		StartCoroutine(PlayingCheck());
 	}
+
+	IEnumerator PlayingCheck(){
+
+		yield return new WaitUntil (() => ((MovieTexture)rightSphere.GetComponent<Renderer> ().material.mainTexture).isPlaying);
+
+
+		IsPlaying = false;
+
+
+	}
+
+}
